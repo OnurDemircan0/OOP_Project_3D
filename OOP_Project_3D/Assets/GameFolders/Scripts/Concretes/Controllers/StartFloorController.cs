ecf24d61ce284dfa9a2ec3025f249568fa0f9835
Onur Demircan <onurdemircan438@gmail.com>
2023-08-28 05:20:27 +0300

    StartFloorController
    StartFloorController added to the StartFloor prefab.

---
+++ OOP_Project_3D/Assets/GameFolders/Scripts/Concretes/Controllers/StartFloorController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OOP_Project_3D.Controllers
+{
+    public class StartFloorController : MonoBehaviour
+    {
+        private void OnCollisionExit(Collision collision)
+        {
+            PlayerController player = collision.collider.GetComponent<PlayerController>();
+
+            if(player != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
+
