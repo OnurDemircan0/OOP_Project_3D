23526608ec05112be3d12a617e08b154cfe29525
Onur Demircan <onurdemircan438@gmail.com>
2023-08-28 05:53:13 +0300

    FinishFloorController
    FinishFloorController added. Particle effects added to the FinishFloor prefab.

---
+++ OOP_Project_3D/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OOP_Project_3D.Controllers
+{
+    public class FinishFloorController : MonoBehaviour
+    {
+        [SerializeField] GameObject _finishLight;
+        [SerializeField] GameObject _finishFireWorks;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            PlayerController player = collision.collider.GetComponent<PlayerController>();
+
+            if (player == null) return;
+
+            if (collision.GetContact(0).normal.y == -1)
+            {
+                _finishLight.SetActive(true);
+                _finishFireWorks.SetActive(true);
+            }
+            else
+            {
+                //Game Over
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
+
+
