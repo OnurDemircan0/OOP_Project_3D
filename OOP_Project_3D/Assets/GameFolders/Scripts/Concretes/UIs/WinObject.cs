19c00cb7ea8ca502670c78f3485545727530dbb2
Onur Demircan <onurdemircan438@gmail.com>
2023-09-01 05:43:37 +0300

    Win Panel
    Win panel added.

---
+++ OOP_Project_3D/Assets/GameFolders/Scripts/Concretes/UIs/WinObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OOP_Project_3D.Managers;
+
+namespace OOP_Project_3D.UIs
+{
+    public class WinObject : MonoBehaviour
+    {
+        [SerializeField] GameObject _winPanel;
+
+        private void Awake()
+        {
+            if (_winPanel.activeSelf)
+            {
+                _winPanel.SetActive(false);
+            }
+        }
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnMissionSucceed += HandleOnMissionSucceed;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnMissionSucceed -= HandleOnMissionSucceed;
+        }
+
+        private void HandleOnMissionSucceed()
+        {
+            if (!_winPanel.activeSelf)
+            {
+                _winPanel.SetActive(true);
+            }
+        }
+    }
+
+}
+
---
+++ OOP_Project_3D/Assets/GameFolders/Scripts/Concretes/UIs/WinPanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OOP_Project_3D.Managers;
+
+namespace OOP_Project_3D.UIs
+{
+    public class WinPanel : MonoBehaviour
+    {
+        public void NextLevelClicked()
+        {
+            GameManager.Instance.LoadLevelScene(1);
+        }
+
+        public void MainMenuClicked()
+        {
+            GameManager.Instance.LoadMenuScene();
+        }
+    }
+}
+
+
